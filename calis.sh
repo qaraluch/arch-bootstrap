6b5999c90e040d9bc51086ddf41723d0a412e4ff
qaraluch <prezenciq@gmail.com>
2019-01-13 10:45:51 +0100

    Refactor calis script

--- calis.sh
+++ calis.sh
@@ -1,267 +1,271 @@
 #!/usr/bin/env bash
-
 # Author: qaraluch - 08.2018 - MIT
-# Part of project name: arch-bootstrap 
+# Part of the repo: arch-bootstrap
 # Custom Arch Linux Installation Script (CALIS)
-# Many thanks to LukeSmithxyz for inspiration!
+# Many thanks to LukeSmithXYZ for inspiration!
 
-################################### VARS ###################################
-readonly HOSTNAME='arch-XXX'
-readonly DEVICE='sda'
-readonly PART_BOOT_SIZE='250'
-readonly PART_SWAP_SIZE='2000'
-readonly PART_ROOT_SIZE='10000'
-readonly CHROOT_SOURCE='https://raw.githubusercontent.com/qaraluch/arch-bootstrap/master/calis-chroot.sh'
-readonly EXEC_PART_MGMT='Y'
-readonly EXEC_INSTALL_ARCH='Y'
-readonly EXEC_DOWN_CHROOT='Y'
-readonly EXEC_CHROOT='Y'
+set -e
 
-################################### UTILS ###################################
-# DELIMITER
-readonly D_APP='[ CALIS ]'
+########################## INSTALLATION PARAMS ##############################################################
+# edit it before run!
+readonly p_hostname='arch-XXX'
+readonly p_device='sda'
+readonly p_part_boot_size='250'
+readonly p_part_swap_size='2000'
+readonly p_part_root_size='10000'
+readonly p_chroot_source='https://raw.githubusercontent.com/qaraluch/arch-bootstrap/master/calis-chroot.sh'
+readonly p_exec_part_mgmt='Y'
+readonly p_exec_install_arch='Y'
+readonly p_exec_down_chroot='Y'
+readonly p_exec_chroot='Y'
+#############################################################################################################
 
-# COLORS
-readonly C_R=$'\033[0;31m'            # Red
-readonly C_G=$'\033[1;32m'            # Green
-readonly C_Y=$'\033[1;33m'            # Yellow
-readonly C_B=$'\033[1;34m'            # Blue
-readonly C_M=$'\033[1;35m'            # Magenta
-readonly C_C=$'\033[1;36m'            # Cyan
-readonly C_E=$'\033[0m'               # End
+# Main
+main () {
+  welcomeMsg
 
-# ICONS
-readonly I_T="[ ${C_G}✔${C_E} ]"      # Tick
-readonly I_W="[ ${C_Y}!${C_E} ]"      # Warn
-readonly I_C="[ ${C_R}✖${C_E} ]"      # Cross
-readonly I_A="[ ${C_Y}?${C_E} ]"      # Ask
+  _switchYN $p_exec_part_mgmt && execPartitionMgmt
+  _switchYN $p_exec_part_mgmt || _echoIt "${_pDel}" "Skipped set up of partitions" "$_ic"
 
-echoIt () {
-  local msg=$1 ; local icon=${2:-''} ; echo "$D_APP$icon $msg" 
+  _switchYN $p_exec_install_arch && execInstallArch
+  _switchYN $p_exec_install_arch || _echoIt "${_pDel}" "Skipped installation of Arch Linux" "$_ic"
+
+  _switchYN $p_exec_down_chroot && execDownloadChroot
+  _switchYN $p_exec_down_chroot || _echoIt "${_pDel}" "Skipped downloading of chroot script" "$_ic"
+
+  _switchYN $p_exec_chroot && execChrootWelcomeMsg
+  _switchYN $p_exec_chroot && execChroot
+  _switchYN $p_exec_chroot || _echoIt "${_pDel}" "Skipped run of chroot script" "$_ic"
+
+  _echoIt "${_pDel}" "ALL DONE!" "$_it"
+  execReboot
 }
 
-errorExit () {
-  echo "$D_APP$I_C $1" 1>&2 ; exit 1
+# Calculated vars
+readonly device_full="/dev/${p_device}"
+readonly part_swap_size_relative=$(( $p_part_swap_size + $p_part_boot_size))
+readonly part_root_size_relative=$(( $p_part_root_size + $part_swap_size_relative))
+readonly part_boot="${device_full}1"
+readonly part_swap="${device_full}2"
+readonly part_root="${device_full}3"
+readonly part_home="${device_full}4"
+
+welcomeMsg() {
+  _echoIt "${_pDel}" "Welcome to: Custom Arch Linux Installation Script (CALIS)"
+  _echoIt "${_pDel}" "Used variables:"
+  _echoIt "${_pDel}" "  - hostname:       $p_hostname"
+  _echoIt "${_pDel}" "  - device:         $p_device"
+  _echoIt "${_pDel}" "    - 1. BOOT (MB): $p_part_boot_size"
+  _echoIt "${_pDel}" "    - 2. SWAP (MB): $p_part_swap_size"
+  _echoIt "${_pDel}" "    - 3. ROOT (MB): $p_part_root_size"
+  _echoIt "${_pDel}" "    - 4. HOME (MB): <the rest of the disk size>"
+  _echoIt "${_pDel}" "  - chroot source:  $p_chroot_source"
+  _echoIt "${_pDel}" "Execution subscript flags:"
+  _echoIt "${_pDel}" "  - run partition management    [Y]es/[N]o: $p_exec_part_mgmt"
+  _echoIt "${_pDel}" "  - run arch installation       [Y]es/[N]o: $p_exec_install_arch"
+  _echoIt "${_pDel}" "  - download chroot script      [Y]es/[N]o: $p_exec_down_chroot"
+  _echoIt "${_pDel}" "  - run chroot script           [Y]es/[N]o: $p_exec_chroot"
+  _echoIt "${_pDel}" "Check above installation settings." "$_iw"
+  _yesConfirmOrAbort "Ready to roll"
 }
 
-errorExitMainScript () {
-  errorExit "${C_R}Sth. went wrong. Aborting script! $C_E"
+execPartitionMgmt () {
+  updateSystemClock
+  createPartitions
+  showPartitionLayout
+  _yesConfirmOrAbort
+  formatPartitionsAndMount
+  _echoIt "${_pDel}" "Partitions are set up."
 }
 
-yesConfirm () {
-  local ABORT_MSG_DEFAULT="Abort script!"
-  local ABORT_MSG=${2:-$ABORT_MSG_DEFAULT}
-  read -p "$D_APP$I_A $1" -n 1 -r
-  echo >&2
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
-  then
-      errorExit "$ABORT_MSG" 
-  fi
-}
-
-pressAnyKey () {
-  read -n 1 -s -r -p "$D_APP Press [any] key to continue."
-  echo >&2
-}
-
-isEqualY () {
-  local STRING=$1
-  [[ "$STRING" == "Y" ]]
-}
-
-isEqualN () {
-  local STRING=$1
-  [[ "$STRING" == "N" ]]
-}
-
-switchYN () {
-  local SWITCH=$1
-  if isEqualY $SWITCH; then
-    return 0
-  else
-    return 1
-  fi
-} 
-
-################################### FNS  ###################################
 updateSystemClock () {
   timedatectl set-ntp true
-  echoIt "Updated system clock." "$I_T"
+  _echoIt "${_pDel}" "Updated system clock." "$_it"
 }
 
 createPartitions () {
-  echoIt "About to create partitions..." 
-  parted --script "${DEVICE_FULL}" -- mklabel msdos \
-    mkpart primary ext4 1Mib "${PART_BOOT_SIZE}MiB" \
+  _echoIt "${_pDel}" "About to create partitions..."
+  parted --script "${device_full}" -- mklabel msdos \
+    mkpart primary ext4 1Mib "${p_part_boot_size}MiB" \
     set 1 boot on \
-    mkpart primary linux-swap "${PART_BOOT_SIZE}MiB" "${PART_SWAP_SIZE_RELATIVE}MiB" \
-    mkpart primary ext4 "${PART_SWAP_SIZE_RELATIVE}MiB" "${PART_ROOT_SIZE_RELATIVE}MiB" \
-    mkpart primary ext4 "${PART_ROOT_SIZE_RELATIVE}MiB" 100%
-} 
+    mkpart primary linux-swap "${p_part_boot_size}MiB" "${part_swap_size_relative}MiB" \
+    mkpart primary ext4 "${part_swap_size_relative}MiB" "${part_root_size_relative}MiB" \
+    mkpart primary ext4 "${part_root_size_relative}MiB" 100%
+}
 
 showPartitionLayout () {
-  parted --script "${DEVICE_FULL}" -- print
-  echoIt "Created partitions." "$I_T"
+  parted --script "${device_full}" -- print
+  _echoIt "${_pDel}" "Created partitions." "$_it"
 }
 
 formatPartitionsAndMount () {
-  echoIt "About to wipe data..." 
-  wipefs "${PART_BOOT}"
-  wipefs "${PART_SWAP}"
-  wipefs "${PART_ROOT}"
-  wipefs "${PART_HOME}"
-  echoIt "About to format partitions..." 
-  mkfs.ext4 ${PART_BOOT}
-  mkfs.ext4 ${PART_ROOT}
-  mkfs.ext4 ${PART_HOME}
-  mkswap ${PART_SWAP}
-  echoIt "Formated partitions." "$I_T"
-  swapon ${PART_SWAP} 
-  mount ${PART_ROOT} /mnt
+  _echoIt "${_pDel}" "About to wipe data..."
+  wipefs "${part_boot}"
+  wipefs "${part_swap}"
+  wipefs "${part_root}"
+  wipefs "${part_home}"
+  _echoIt "${_pDel}" "About to format partitions..."
+  mkfs.ext4 ${part_boot}
+  mkfs.ext4 ${part_root}
+  mkfs.ext4 ${part_home}
+  mkswap ${part_swap}
+  _echoIt "${_pDel}" "Formated partitions." "$_it"
+  swapon ${part_swap}
+  mount ${part_root} /mnt
   mkdir -p /mnt/boot
-  mount ${PART_BOOT} /mnt/boot
+  mount ${part_boot} /mnt/boot
   mkdir -p /mnt/home
-  mount ${PART_HOME} /mnt/home
-  echoIt "Mounted partitions." "$I_T"
+  mount ${part_home} /mnt/home
+  _echoIt "${_pDel}" "Mounted partitions." "$_it"
+}
+
+execInstallArch () {
+  _echoIt "${_pDel}" "About to install Arch Linux."
+  _pressAnyKey
+  installArch
+  generateFstabFile
+  setupHostName
 }
 
 installArch () {
   pacstrap /mnt base base-devel
-  echoIt "Installed Arch." "$I_T"
+  _echoIt "${_pDel}" "Installed Arch." "$_it"
 }
 
 generateFstabFile () {
   genfstab -U /mnt >> /mnt/etc/fstab
-  echoIt "Generated fstab file." "$I_T"
-  echoIt "See fstab file:" 
+  _echoIt "${_pDel}" "Generated fstab file." "$_it"
+  _echoIt "${_pDel}" "See fstab file:"
   more /mnt/etc/fstab
 }
 
 setupHostName () {
-  echo $HOSTNAME > /mnt/etc/hostname 
+  echo $p_hostname > /mnt/etc/hostname
   cat <<EOT >> /mnt/etc/hosts
 127.0.0.1	localhost
 ::1		localhost
 # 127.0.1.1	myhostname.localdomain	myhostname
 EOT
-  # sed -i "8i 127.0.1.1\t$HOSTNAME.localdomain\t$HOSTNAME" /etc/hosts
-  echoIt "Setup hostname." "$I_T"
+  # sed -i "8i 127.0.1.1\t$p_hostname.localdomain\t$p_hostname" /etc/hosts
+  _echoIt "${_pDel}" "Setup hostname." "$_it"
+}
+
+execDownloadChroot () {
+  _echoIt "${_pDel}" "About to download calis-chroot.sh script..."
+  downloadChrootScript
 }
 
 downloadChrootScript () {
-  curl -sL "${CHROOT_SOURCE}" > /mnt/chroot.sh
-  echoIt "Download completed!" "$I_T"
+  curl -sL "${p_chroot_source}" > /mnt/chroot.sh
+  _echoIt "${_pDel}" "Download completed!" "$_it"
+}
+
+execChrootWelcomeMsg() {
+  _echoIt "${_pDel}" "Chroot script is downloaded." "$_it"
+  _echoIt "${_pDel}" "If you need edit some of this settings:"
+  _echoIt "${_pDel}" "  - locale"
+  _echoIt "${_pDel}" "  - timezone"
+  _echoIt "${_pDel}" "  - keyboard"
+  _echoIt "${_pDel}" "  - bootloader"
+  _echoIt "${_pDel}" "  - network manager"
+  _echoIt "${_pDel}" "Abort this script and edit file /mnt/chroot.sh"
+  _echoIt "${_pDel}" "Re-run only execution of chroot of CALIS script afterwords."
+  _yesConfirmOrAbort "Ready to roll"
+}
+
+execChroot () {
+  _echoIt "${_pDel}" "Run arch-chroot..."
+  runChroot
 }
 
 runChroot () {
-  arch-chroot /mnt bash chroot.sh ${DEVICE_FULL} && rm /mnt/chroot.sh
-  echoIt "Chroot script ended. Clean it up too." "$I_T"
+  arch-chroot /mnt bash chroot.sh ${device_full} && rm /mnt/chroot.sh
+  _echoIt "${_pDel}" "Chroot script ended. Clean it up too." "$_it"
+}
+
+execReboot () {
+  _echoIt "${_pDel}" "We are ready to reboot to brand new Arch Linux System..."
+  _echoIt "${_pDel}" "Fingers crossed!"
+  rebootNow
+  orGoBackToChroot
 }
 
 rebootNow () {
-  read -p "$D_APP$I_A Confirm reboot or skip it [y/n]?" -n 1 -r
+  read -p "$D_APP$_ia Confirm reboot or skip it [y/n]?" -n 1 -r
   echo >&2
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
     umount -R /mnt
-    swapoff ${PART_SWAP}
+    swapoff ${part_swap}
     shutdown -h now
   fi
 }
 
 orGoBackToChroot () {
-  read -p "$D_APP$I_A Maybe go to chroot again or skip it [y/n]?" -n 1 -r
+  read -p "$D_APP$_ia Maybe go to chroot again or skip it [y/n]?" -n 1 -r
   echo >&2
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-    arch-chroot /mnt 
+    arch-chroot /mnt
   fi
-  clear 
-  echoIt "Nothing more to do... :("
+  clear
+  _echoIt "${_pDel}" "Nothing more to do... :("
 }
 
-### Calculated vars:
-readonly DEVICE_FULL="/dev/${DEVICE}"
-readonly PART_SWAP_SIZE_RELATIVE=$(( $PART_SWAP_SIZE + $PART_BOOT_SIZE))
-readonly PART_ROOT_SIZE_RELATIVE=$(( $PART_ROOT_SIZE + $PART_SWAP_SIZE_RELATIVE))
-readonly PART_BOOT="${DEVICE_FULL}1"
-readonly PART_SWAP="${DEVICE_FULL}2"
-readonly PART_ROOT="${DEVICE_FULL}3"
-readonly PART_HOME="${DEVICE_FULL}4"
 
-################################### MAIN ###################################
-main () {
-  echoIt "Welcome to: Custom Arch Linux Installation Script (CALIS)"
-  echoIt "Used variables:"
-  echoIt "  - hostname:       $HOSTNAME"
-  echoIt "  - device:         $DEVICE"
-  echoIt "    - 1. BOOT (MB): $PART_BOOT_SIZE"
-  echoIt "    - 2. SWAP (MB): $PART_SWAP_SIZE"
-  echoIt "    - 3. ROOT (MB): $PART_ROOT_SIZE"
-  echoIt "    - 4. HOME (MB): <the rest of the disk size>"
-  echoIt "  - chroot source:  $CHROOT_SOURCE"
-  echoIt "Execution subscript flags:"
-  echoIt "  - run partition management    [Y]es/[N]o: $EXEC_PART_MGMT"
-  echoIt "  - run arch installation       [Y]es/[N]o: $EXEC_INSTALL_ARCH"
-  echoIt "  - download chroot script      [Y]es/[N]o: $EXEC_DOWN_CHROOT"
-  echoIt "  - run chroot script           [Y]es/[N]o: $EXEC_CHROOT"
-  echoIt "Check above installation settings." "$I_W"
-  yesConfirm "Ready to roll [y/n]? " 
+# Utils
+readonly _pDel='[ CALIS ]'
+
+export _cr=$'\033[0;31m'            # color red
+export _cg=$'\033[1;32m'            # color green
+export _cy=$'\033[1;33m'            # color yellow
+export _cb=$'\033[1;34m'            # color blue
+export _cm=$'\033[1;35m'            # color magenta
+export _cc=$'\033[1;36m'            # color cyan
+export _ce=$'\033[0m'               # color end
+
+export _it="[ ${_cg}✔${_ce} ]"        # icon tick
+export _iw="[ ${_cy}!${_ce} ]"       # icon warn
+export _ic="[ ${_cr}✖${_ce} ]"      # icon cross
+export _ia="[ ${_cy}?${_ce} ]"      # icon ask
+
+_echoIt() {
+  local delimiter=$1 ; local msg=$2 ; local icon=${3:-''} ; echo "${delimiter}${icon} $msg" >&2
 }
 
-execPartitionMgmt () {
-  updateSystemClock || errorExitMainScript
-  createPartitions || errorExitMainScript
-  showPartitionLayout || errorExitMainScript
-  yesConfirm "Continue... [y/n]? " 
-  formatPartitionsAndMount || errorExitMainScript
-  echoIt "Partitions are set up."
+_errorExit() {
+  local delimiter=$1 ; local msg=$2 ; local icon=${3:-"$_ic"} ; echo "${delimiter}${icon} ${msg}" 1>&2 ; exit 1
 }
 
-execInstallArch () {
-  echoIt "About to install Arch Linux."
-  pressAnyKey
-  installArch || errorExitMainScript 
-  generateFstabFile || errorExitMainScript
-  setupHostName || errorExitMainScript
+_yesConfirmOrAbort() {
+  local msg=${1:-'Continue'}
+  local msgDefaultAbort=${2:-'Abort script!'}
+  read -n 1 -s -r -p "${_pDel}${_ia} ${msg} [Y/n]?"
+  echo >&2
+  REPLY=${REPLY:-'Y'}
+  if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
+    _errorExit "${_pDel}" "${msgDefaultAbort}"
+  fi
 }
 
-execDownloadChroot () {
-  echoIt "About to download calis-chroot.sh script..." 
-  downloadChrootScript || errorExitMainScript 
-  echoIt "Chroot script is downloaded." "$I_T"
-  echoIt "If you need edit some of this settings:" 
-  echoIt "  - locale"
-  echoIt "  - timezone"
-  echoIt "  - keyboard"
-  echoIt "  - bootloader"
-  echoIt "  - network manager"
-  echoIt "Abort this script and edit file /mnt/chroot.sh"
-  echoIt "Re-run only execution of chroot of CALIS script afterwords."
-  yesConfirm "Continue... [y/n]? " 
+_pressAnyKey() {
+  read -n 1 -s -r -p "${_pDel}${_ia} Press [any] key to continue. "
+  echo >&2
 }
 
-execChroot () {
-  echoIt "Run arch-chroot..."
-  runChroot || errorExitMainScript
+_isStringEqualY() {
+  local string=$1
+  [[ "$string" == "Y" ]]
 }
 
-execReboot () {
-  echoIt "We are ready to reboot to brand new Arch Linux System..."
-  echoIt "Fingers crossed!"
-  rebootNow
-  orGoBackToChroot
+_switchYN() {
+  local switch=$1
+  if _isStringEqualY $switch; then
+    return 0
+  else
+    return 1
+  fi
 }
 
-#run it!
-main 
-switchYN $EXEC_PART_MGMT && execPartitionMgmt
-switchYN $EXEC_PART_MGMT || echoIt "Skipped set up of partitions" "$I_C"
-switchYN $EXEC_INSTALL_ARCH && execInstallArch
-switchYN $EXEC_INSTALL_ARCH || echoIt "Skipped installation of Arch Linux" "$I_C"
-switchYN $EXEC_DOWN_CHROOT && execDownloadChroot
-switchYN $EXEC_DOWN_CHROOT || echoIt "Skipped downloading of chroot script" "$I_C"
-switchYN $EXEC_CHROOT && execChroot
-switchYN $EXEC_CHROOT || echoIt "Skipped run of chroot script" "$I_C"
-echoIt "ALL DONE!" "$I_T"
-execReboot
+# Main run!
+main
